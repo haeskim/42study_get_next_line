360e663a03298b4c9183beb77cb89900c565a15c
haeskim <117874187+haeskim@users.noreply.github.com>
2023-01-06 11:35:20 +0900

    sdf
    pdf

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: haeskim <haeskim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 09:47:50 by haeskim           #+#    #+#             */
-/*   Updated: 2022/12/29 19:25:51 by haeskim          ###   ########.fr       */
+/*   Updated: 2023/01/06 10:58:06 by haeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,123 +17,160 @@
 
 char	*get_next_line(int fd)
 {
-	static char	*aa;//read후에 남은 애들 들어갈 자리
-	char		buf[BUFFER_SIZE];
+	static char	remain[BUFFER_SIZE + 1];
+	char		buf[BUFFER_SIZE + 1];
 	char		*line;
 	int			i;
 	int			j;
 	int			read_size;
+	char		*tmp;
+	char		tmp2[BUFFER_SIZE + 1];
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	if (!aa)
-	{
-		aa = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!aa)
-			return (0);
-	}
-
 	line = (char *)malloc(sizeof(char));
-	*line = 0;
-
-	// line = (char *)malloc(sizeof(char));
-	// if (!line)
-	// {
-	// 	free(aa);
-	// 	return (0);
-	// }
-/*
-	if (aa)
+	if (!line)
+		return (0);
+	line[0] = 0;
+	if (remain[0])
 	{
-		if (ft_strchr(aa, '\n'))
+		i = 0;
+		j = 0;
+		if (ft_strchr(remain, '\n'))
 		{
-			i = 0;
-			j = 0;
-			while (aa[i] != '\n')
+			while (remain[i] && remain[i] != '\n')
 			{
-				line[i] = aa[i];
+				tmp2[i] = remain[i];
 				i++;
 			}
-			line[i] = aa[i];
+			tmp2[i] = remain[i];
 			i++;
-			while (aa[i])
-				aa[j++] = aa[i++];
-			aa[j] = 0;
+			tmp2[i] = 0;
+			while (remain[i])
+				remain[j++] = remain[i++];
+			remain[j] = 0;
+			free(line);
+			line = ft_strdup(tmp2);
 			return (line);
 		}
-		line = ft_strjoin(line, aa);
-	}
-*/
-	if (aa)
-	{
-		if (ft_strchr(aa, '\n'))
-		{
-			i = 0;
-			j = 0;
-			while (aa[i] != '\n')
-				i++;
-			line = (char *)malloc(sizeof(char) * i);
-			i = 0;
-			while (aa[i] != '\n')
-			{
-				line[i] = aa[i];
-				i++;
-			}
-			line[i] = aa[i];
-			i++;
-			while (aa[i])
-				aa[j++] = aa[i++];
-			aa[j] = 0;
-			return (line);
-		}
-		line = ft_strjoin(line, aa);
+		free (line);
+		line = ft_strdup(remain);
+		if (!line)
+			return (0);
+		remain[0] = 0;
 	}
 	read_size = 1;
 	while (read_size > 0)
 	{
 		read_size = read(fd, buf, BUFFER_SIZE);
-		if (ft_strchr(buf, '\n'))
-			break ;
 		if (read_size <= -1)
 		{
-			free(aa);
+			if (line)
+				free(line);
+			return (0);
+		}
+		buf[read_size] = 0;
+		if (ft_strchr(buf, '\n') || read_size < BUFFER_SIZE || !buf[0])
+			break ;
+		if (!line[0])
+		{	
+			free(line);
+			line = ft_strdup(buf);
+			if (!line)
+				return (0);
+		}
+		else
+		{
+			tmp = line;
+			line = ft_strjoin(line, buf);
+			if (!line)
+			{
+				free(tmp);
+				return (0);
+			}
+			free(tmp);
+		}
+	}
+	if (read_size == 0)
+	{
+		if (!line[0])
+		{
 			free(line);
 			return (0);
 		}
-		line = ft_strjoin(line, buf);
-	}
-	if (read_size == 0)
 		return (line);
-	j = 0;
-	while (line[j])
-		j++;
+	}
 	i = 0;
-	while (buf[i] != '\n')
-		line[j++] = buf[i++];
-	line[j++] = buf[i++];
+	if (read_size < BUFFER_SIZE)
+	{
+		while (buf[i] && buf[i] != '\n')
+		{
+			tmp2[i] = buf[i];
+			i++;
+		}
+		tmp2[i] = buf[i];
+		i++;
+		tmp2[i] = 0;
+		if (!line[0])
+		{
+			free(line);
+			line = ft_strdup(tmp2);
+			if (!line)
+				return (0);
+		}
+		else
+		{
+			tmp = line;
+			line = ft_strjoin(line, tmp2);
+			if (!line)
+			{
+				free(tmp);
+				return (0);
+			}
+			free(tmp);
+		}
+		j = 0;
+		if (buf[i - 1] == '\n')
+		{
+			while (buf[i])
+			{
+				remain[j++] = buf[i++];
+			}
+			remain[j] = 0;
+		}
+		return (line);
+	}
+	while (buf[i] && buf[i] != '\n')
+	{
+		tmp2[i] = buf[i];
+		i++;
+	}
+	tmp2[i] = buf[i];
+	i++;
+	tmp2[i] = 0;
+	if (!line[0])
+	{
+		free(line);
+		line = ft_strdup(tmp2);
+		if (!line)
+			return (0);
+	}
+	else
+	{
+		tmp = line;
+		line = ft_strjoin(line, tmp2);
+		if (!line)
+		{
+			free(tmp);
+			return (0);
+		}
+		free(tmp);
+	}
 	j = 0;
 	while (buf[i])
 	{
-		aa[j++] = buf[i++];
+		remain[j++] = buf[i++];
 	}
-//	aa[j] = '\0';
+	remain[j] = 0;
 	return (line);
-}
-
-#include <fcntl.h>
-
-int	main(void)
-{
-	char	*buf;
-	int		fd;
-
-	fd = open("/Users/haeskim/haeskim/gnl/file1.txt", O_RDONLY);
-	buf = get_next_line(fd);
-	printf("==1==%s", buf);
-	buf = get_next_line(fd);
-	printf("==2==%s", buf);
-	buf = get_next_line(fd);
-	printf("==3==%s", buf);
-//	buf = get_next_line(fd);
-//	printf("==4==%s", buf);
 }
